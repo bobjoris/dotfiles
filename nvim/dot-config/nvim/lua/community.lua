34bff4e1bee49863fd3b215449aa2525d733de23
Joris Girardot <joris@girardot.eu>
2025-02-19 17:28:59 +0100

    use dot- prefix

---
+++ nvim/dot-config/nvim/lua/community.lua
@@ -0,0 +1,17 @@
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+	"AstroNvim/astrocommunity",
+	{ import = "astrocommunity.git.git-blame-nvim" },
+	{ import = "astrocommunity.completion.copilot-cmp" },
+	{ import = "astrocommunity.utility.noice-nvim" },
+	{ import = "astrocommunity.recipes.cache-colorscheme" },
+	{ import = "astrocommunity.colorscheme.bamboo-nvim" },
+	{ import = "astrocommunity.colorscheme.catppuccin" },
+	{ import = "astrocommunity.colorscheme.nightfox-nvim" },
+	{ import = "astrocommunity.colorscheme.vscode-nvim" },
+	-- import/override with your plugins folder
+}
---
+++ nvim/dot-config/nvim/lua/plugins/mappings.lua
@@ -0,0 +1,16 @@
+return {
+	{
+		"AstroNvim/astrocore",
+		---@type AstroCoreOpts
+		opts = {
+			mappings = {
+				n = {
+					["<C-d>"] = { "<C-d>zz" },
+					["<C-f>"] = { "<C-f>zz" },
+					["<C-u>"] = { "<C-u>zz" },
+					["<C-b>"] = { "<C-b>zz" },
+				},
+			},
+		},
+	},
+}
